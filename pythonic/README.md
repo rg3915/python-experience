17b489dc494e2dc195d46e69b7e23164bc63925e
rg3915 <rg3915@yahoo.com.br>
2016-05-11 21:58:04 -0300

    Pythonic TODO

---
+++ pythonic/README.md
@@ -0,0 +1,34 @@
+# Objetos Pythônicos
+
+Apresentação do Luciano Ramalho no Garoa Hacker Club dia 11/05/16.
+
+
+### Tamanho com `len()`
+
+```bash
+len(s), len(L)
+s.__len__(), L.__len__()
+```
+
+### Aritmética
+
+```bash
+a = 2
+b = 3
+a * b, a.__mul__(b)
+```
+
+```bash
+L = [10, 20, 30]
+for i in L:
+    print(i, end=' ')
+```
+
+```bash
+$ python -m doctest vector0.py -v
+$ python -m doctest vector0.py -o FAIL_FAST
+```
+
+https://speakerdeck.com/ramalho/objetos-pythonicos-1
+
+https://github.com/fluentpython/pythonic-api/blob/master/examples/vector0.py
---
+++ pythonic/vector0.py
@@ -0,0 +1,42 @@
+'''
+Vector: classe vetor euclidiano
+
+    >>> v1 = Vector([1, 2])
+    >>> v1
+    Vector([1.0, 2.0])
+    >>> format(v1)
+    '(1.0, 2.0)'
+    >>> format(v1, '.2f')
+    '(1.00, 2.00)'
+    >>> format(v1, '.3e')
+    '(1.000e+00, 2.000e+00)'
+'''
+
+from array import array
+import math
+
+
+class Vector:
+    typecode = 'd'
+
+    def __init__(self, components):
+        self._components = array(self.typecode, components)
+
+    def __len__(self):
+        return len(self._components)
+
+    def __iter__(self):
+        return iter(self._components)
+
+    def __abs__(self):
+        return math.sqrt(sum(x * x for x in self))
+
+    def __eq__(self, other):
+        return (len(self) == len(other) and
+                all(a == b for a, b in zip(self, other)))
+
+    def __repr__(self):
+        return 'Vector({})'.format(list(self))
+
+    def __format__(self, format_spec):
+        return '({})'.format(', '.join(format(v, format_spec) for v in self))
