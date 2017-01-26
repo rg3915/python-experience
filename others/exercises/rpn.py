6a84af223613ec949cdaf87b703bd6e386478e50
rg3915 <rg3915@yahoo.com.br>
2017-01-26 14:02:50 -0200

    Mock on rpn.py

--- others/exercises/rpn.py
+++ others/exercises/rpn.py
@@ -1,9 +1,28 @@
+from unittest.mock import patch
+
+
 # Notação polonesa reversa
+
 
 '''
 >>>
 40 2 +
 42
+>>>
+40 2 -
+38
+>>>
+40 2 *
+80
+>>>
+40 2 //
+20
+>>>
+40 2 /
+20
+>>>
+40 2 %
+0
 '''
 
 
@@ -21,6 +40,12 @@ def rpn():
     }
     dunder_method = switch.get(sinal)
     return getattr(a, dunder_method)(b)
+
+
+def test_rpn():
+    with patch('__main__.input') as mocked_input:
+        mocked_input.return_value = '40 2 +'
+        assert rpn() == 42
 
 
 if __name__ == '__main__':
