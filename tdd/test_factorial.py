edda303cb499550f52c696efa8800a843e053196
rg3915 <rg3915@yahoo.com.br>
2017-01-29 20:50:47 -0200

    AutoPEP 8

--- tdd/test_factorial.py
+++ tdd/test_factorial.py
@@ -3,13 +3,13 @@ def factorial(n):
 
     for i in range(1, n + 1):
         total *= i
-    
+
     return total
 
 
 def test_factorial():
-	assert factorial(0) == 1
-	assert factorial(1) == 1
-	assert factorial(3) == 6
-	assert factorial(4) == 24
-	assert factorial(5) == 120
+    assert factorial(0) == 1
+    assert factorial(1) == 1
+    assert factorial(3) == 6
+    assert factorial(4) == 24
+    assert factorial(5) == 120
