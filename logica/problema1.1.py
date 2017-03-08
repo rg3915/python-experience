80ca53ef20dc621bd8e542ae9f43162482f319aa
rg3915 <rg3915@yahoo.com.br>
2017-03-08 16:43:29 -0300

    Refatorando problema1.1.py

--- logica/problema1.1.py
+++ logica/problema1.1.py
@@ -1,14 +1,20 @@
 '''
-Problema 1.1 - Dada uma sequência de números inteiros diferentes de zero, terminada por um zero, calcular a sua soma. 
-Por exemplo, para a sequência 1274-680 o seu programa deve escrever o número 35.
+Problema 1.1 - Dada uma sequência de números inteiros diferentes de zero,
+terminada por um zero, calcular a sua soma.
+Por exemplo, para a sequência 12, 7, 4, -6, 8, 0 o seu programa deve escrever
+o número 25 (retificação).
 
 Solução:
 '''
+
 soma = 0
 numero = int(input())
-print(numero) 
+
+print(numero)
+
 while (numero != 0):
-	soma = soma + numero
-	numero = int(input())
-	print(numero)
+    soma += numero
+    numero = int(input())
+    print(numero)
+
 print(soma)
