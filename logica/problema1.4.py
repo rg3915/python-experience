337c4ee8605d1935315fda496682ae56c9297356
rg3915 <rg3915@yahoo.com.br>
2017-03-08 16:52:26 -0300

    Refatorando problema1.4.py

--- logica/problema1.4.py
+++ logica/problema1.4.py
@@ -1,9 +1,9 @@
 '''
 Problema 1.4 - Dados um número inteiro n >= 0, e uma sequência com n inteiros,
-determinar a soma dos inteiros positivos da sequência. Por exemplo, para a sequência 6 -2 7 0 -5 8 4 
-o programa deve escrever o número 25. 
+determinar a soma dos inteiros positivos da sequência. Por exemplo, para
+a sequência 6, -2, 7, 0, -5, 8, 4 o programa deve escrever o número 25.
 
-(Obs: na apostila diz que o resultado é 19, mas somando deu 25)
+Note que no exemplo n = 6. E a sequencia é -2, 7, 0, -5, 8, 4.
 
 Solução:
 '''
@@ -11,9 +11,9 @@ Solução:
 n = int(input("Digite o comprimento da sequência: "))
 contador = 0
 soma = 0
-while(contador < n):
-	numero = int(input("Digite o próximo número: "))
-	if(numero > 0):
-		soma = soma + numero
-	contador = contador + 1
+while contador < n:
+    numero = int(input("Digite o próximo número: "))
+    if numero > 0:
+        soma = soma + numero
+    contador = contador + 1
 print("A soma dos inteiros positivos é", soma)
