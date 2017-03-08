a7648594f12fe0b3586b5a738ff5013203cdafc6
Leila Aparecida da Silva <leilaapsilva@outlook.com.br>
2017-03-08 14:34:51 -0300

    Create problema1.1.py

---
+++ logica/problema1.1.py
@@ -0,0 +1,5 @@
+'''
+Problema 1.1 - Dada uma sequência de números inteiros diferentes de zero, terminada por um zero, calcular a sua soma. Por exemplo, para a sequência 1274-680 o seu programa deve escrever o número 35.
+
+Solução:
+'''
