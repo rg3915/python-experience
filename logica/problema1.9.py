efb43bc48338d963e2b0a77440803899a9df606d
rg3915 <rg3915@yahoo.com.br>
2017-03-28 12:51:22 -0300

    Renomeando arquivos

--- logica/problema1.9.py
+++ logica/problema1.9.py
@@ -3,7 +3,6 @@ Problema 1.9 - Dados um número inteiro n > 0 e uma sequência com n números in
 verificar se a sequência esta em ordem crescente
 
 Solução:
-
 '''
 
 n = int(input("Insira a quantidade de números: "))
@@ -13,12 +12,10 @@ anterior = int(input("Digite o primeiro número: "))
 for i in range(n - 1):
     numero = int(input("Digite o próximo número: "))
     if (anterior >= numero):
-            crescente = 0;
+        crescente = 0
     anterior = numero
 
 if crescente == 1:
     print("A sequência é crescente")
 else:
-    print("A sequêcia não é crescente")
-    
-
+    print("A sequência não é crescente")
