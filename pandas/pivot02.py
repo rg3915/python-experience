5e72eb7fef6884c28a0264e94b6fdb4f7936fe8b
rg3915 <rg3915@yahoo.com.br>
2017-07-05 22:11:10 -0300

    Pandas examples

---
+++ pandas/pivot02.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+
+questions = [1, 3, 9, 6, 7, 5, 4, 2, 8, 10, ]
+answers = [
+    ("user1", {"q10": "39", "q1": "1", "q3": "9", "q2": "5", "q5": "18", "q4": "16", "q7": "25", "q6": "21", "q9": "35", "q8": "31"}),
+    ("user2", {"q10": "40", "q1": "2", "q3": "10", "q2": "6", "q5": "19", "q4": "17", "q7": "26", "q6": "22", "q9": "36", "q8": "32"}),
+    ("user3", {"q10": "41", "q1": "3", "q3": "11", "q2": "7", "q5": "20", "q4": "18", "q7": "27", "q6": "23", "q9": "37", "q8": "33"}),
+]
+answers_data = []
+for user, qa in answers:
+    for q, a in sorted(qa.items()):
+        answers_data.append(dict(user=user, question=q, answer=a))
+
+df = pd.DataFrame(answers_data)
+pv = df.pivot(index='user', columns='question', values='answer')
+print(pv)
